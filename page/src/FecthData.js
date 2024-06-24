e0d57d270857705a6285d5370c5f23e8288d84f7
victorsga22 <122573601+victorsga22@users.noreply.github.com>
2024-06-24 22:34:20 +0200

    init commit

---
+++ page/src/FecthData.js
@@ -0,0 +1,44 @@
+// src/FetchData.js
+import React, { useEffect, useState } from 'react';
+
+const FetchData = () => {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('https://aplicaciones.educa.jcyl.es/PITIA/rest/api/participantes/216368/llamamientos'); // Reemplaza con la URL de tu API
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        const jsonData = await response.json();
+        setData(jsonData);
+      } catch (error) {
+        setError(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
+
+  return (
+    <div>
+      <h1>Data from API</h1>
+      <pre>{JSON.stringify(data, null, 2)}</pre>
+    </div>
+  );
+};
+
+export default FetchData;
